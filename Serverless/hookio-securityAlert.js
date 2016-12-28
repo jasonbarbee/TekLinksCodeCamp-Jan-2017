340997b2184b77512eeadae216543372034b9b0d
Jason Barbee <jason.barbee@gmail.com>
2016-12-27 18:07:17 -0600

    tropo

--- Serverless/hookio-securityAlert.js
+++ Serverless/hookio-securityAlert.js
@@ -1,16 +1,39 @@
 module['exports'] = function myService (hook) {
   var text = '';
+  var results;
 if(hook.params.version == "VyOS") {
   text = "Security Alert - INFO: " + " IP:" + hook.params.ip + " Hostname:" + hook.params.hostname + " Version:" + hook.params.version + " violates security policy. Opening Connectwise Ticket.";
   // Whoa this is really serious. We have strict rules about this kind of thing.
   // Call Tropo IVR to call too..
+  https://api.tropo.com/1.0/sessions?action=create&token=754147586a6a6645646f664a77457378736f7364584d46786c4f44586870504d706c4543444764486277545a&numberToDial=2052495988&customerName=jeremy&msg=test
+var request = require('request');
+request({
+    url: 'https://api.tropo.com/1.0/sessions',
+    qs: {
+      action: "create",
+      token: "754147586a6a6645646f664a77457378736f7364584d46786c4f44586870504d706c4543444764486277545a",
+      customerName: "JasonBarbee",
+      numberToDial: "2517676234",
+      msg: text
+    	},
+    method: 'GET',
+    body: ''
+}, function(error, response, body){
+    if(error) {
+        console.log(error);
+        hook.res.write("Tropo Call Failed\n");
+    } else {
+        console.log(response.statusCode, body);
+        hook.res.write("Tropo Call Successful\n");
+    }
+});
 }
  else {
-    return hook.res.end("No Security issues found");
+    return hook.res.write("No Security issues found\n");
  }
   
 //Log this to Spark  
-    //Load the request module
+//Load the request module
 var request = require('request');
 request({
     url: 'https://hook.io/' + hook.params.hookname + '/sparklogger', //URL to hit
@@ -24,10 +47,12 @@ request({
 }, function(error, response, body){
     if(error) {
         console.log(error);
+        hook.res.write("Spark Log Failed\n");
     } else {
         console.log(response.statusCode, body);
-      hook.res.end("Spark Alert Sent");
+        hook.res.write("Spark Log Successful\n");
     }
 });
+hook.res.end();
    
 };
