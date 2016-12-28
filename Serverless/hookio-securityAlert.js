d8b743e7c7324aa26acd006711033ca80de62f1c
Jason Barbee <jason.barbee@gmail.com>
2016-12-27 19:04:34 -0600

    tropo works now

--- Serverless/hookio-securityAlert.js
+++ Serverless/hookio-securityAlert.js
@@ -1,38 +1,15 @@
 module['exports'] = function myService (hook) {
   var text = '';
-  var results;
+  var results = '';
 if(hook.params.version == "VyOS") {
   text = "Security Alert - INFO: " + " IP:" + hook.params.ip + " Hostname:" + hook.params.hostname + " Version:" + hook.params.version + " violates security policy. Opening Connectwise Ticket.";
   // Whoa this is really serious. We have strict rules about this kind of thing.
   // Call Tropo IVR to call too..
-  //https://api.tropo.com/1.0/sessions?action=create&token=754147586a6a6645646f664a77457378736f7364584d46786c4f44586870504d706c4543444764486277545a&numberToDial=2052495988&customerName=jeremy&msg=test
-var troporequest = require('request');
-troporequest({
-    url: 'https://api.tropo.com/1.0/sessions',
-    qs: {
-      action: "create",
-      token: "754147586a6a6645646f664a77457378736f7364584d46786c4f44586870504d706c4543444764486277545a",
-      customerName: "JasonBarbee",
-      numberToDial: "2517676234",
-      msg: text
-    	},
-    method: 'GET',
-    body: ''
-}, function(error, response, body){
-    if(error) {
-        console.log(error);
-        hook.res.write("Tropo Call Failed\n");
-    } else {
-        console.log(response.statusCode, body);
-        hook.res.write("Tropo Call Successful\n");
-    }
-});
-
-
+console.log("About to Spark Log");
 //Log this to Spark  
 //Load the request module
-var sparkrequest = require('request');
-sparkrequest({
+var request = require('request');
+request({
     url: 'https://hook.io/' + hook.params.hookname + '/sparklogger', //URL to hit
     qs: {
       bottoken: hook.params.bottoken, 
@@ -44,16 +21,37 @@ sparkrequest({
 }, function(error, response, body){
     if(error) {
         console.log(error);
-        hook.res.write("Spark Log Failed\n");
+        return hook.res.end("Spark Log Failed!\n");
     } else {
         console.log(response.statusCode, body);
-        hook.res.write("Spark Log Successful\n");
+        return hook.res.end("Spark Log Successful\n");
     }
 });
 
+  // Let's make an IVR call to alert our staff.
+request({
+    url: 'https://api.tropo.com/1.0/sessions',
+    qs: {
+      action: 'create',
+      token: '754147586a6a6645646f664a77457378736f7364584d46786c4f44586870504d706c4543444764486277545a',
+      customerName: hook.params.customername,
+      numberToDial: hook.params.numbertocall,
+      msg: text
+    	},
+    method: 'GET',
+    body: ''
+}, function(error, response, body){
+    if(error) {
+        console.log(error);
+        return hook.res.write("Tropo Call Failed\n");
+    } else {
+        console.log(response.statusCode, body);
+        return hook.res.write("Tropo Call Successful\n");
+    }
+});
 }
  else {
-    hook.res.write("No Security issues found\n");
+    return hook.res.end("No Security issues found\n");
  }
-return hook.res.end(); 
+//return hook.res.end(results); 
 };
