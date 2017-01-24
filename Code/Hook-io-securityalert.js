5fd48f06576b6f8d3530383e73935ace68f739c2
Jason Barbee <jason.barbee@gmail.com>
2017-01-23 18:54:13 -0600

    final work

--- Code/Hook-io-securityalert.js
+++ Code/Hook-io-securityalert.js
@@ -1,52 +1,56 @@
 module['exports'] = function myService (hook) {
   var text = '';
   var results = '';
+  // console.log("Version " + hook.params.version);
+  // console.log("Token " + hook.params.tropotoken);
+  // console.log(hook.params);
+  
 if(hook.params.version == "VyOS") {
-  text = "Security Alert - INFO: " + " IP:" + hook.params.ip + " Hostname:" + hook.params.hostname + " Version:" + hook.params.version + " violates security policy.";
-  // Whoa this is really serious. We have strict rules about this kind of thing.
-  // Call Tropo IVR to call too..
-console.log("About to Spark Log");
-//Log this to Spark  
-//Load the request module
-var request = require('request');
-request({
-    url: 'https://hook.io/' + hook.params.hookname + '/sparklogger', //URL to hit
-    qs: {
-      bottoken: hook.params.bottoken, 
-      roomid: hook.params.roomid,
-      message: text
-    	},
-    method: 'GET',
-    body: ''
-}, function(error, response, body){
-    if(error) {
-        console.log(error);
-        return hook.res.end("Spark Log Failed!\n");
-    } else {
-        console.log(response.statusCode, body);
-        return hook.res.end("Spark Log Successful\n");
-    }
-});
+  text = "Hook.io - Security Alert: " + " IP:" + hook.params.ip + " Hostname:" + hook.params.hostname + " Version:" + hook.params.version + " violates security policy.";
+  	console.log("About to Spark Log");
+	//Log this to Spark  
 
+  var request = require('request');
+  request({
+      url: 'https://hook.io/' + hook.params.hookname + '/sparklogger', //URL to hit
+      qs: {
+        bottoken: hook.params.bottoken, 
+        roomid: hook.params.roomid,
+        message: text
+          },
+      method: 'GET',
+      body: ''
+  }, function(error, response, body){
+      if(error) {
+          console.log(error);
+          return hook.res.end("Spark Log Failed!\n");
+      } else {
+          console.log(response.statusCode, body);
+          return hook.res.end("Spark Log Successful\n");
+      }
+  });
+
+  console.log("About to send Tropo");
   // Let's make an IVR call to alert our staff.
 request({
     url: 'https://api.tropo.com/1.0/sessions',
     qs: {
       action: 'create',
       token: hook.params.tropotoken,
-      customerName: hook.params.customername,
       numberToDial: hook.params.numbertocall,
       msg: text
     	},
     method: 'GET',
     body: ''
 }, function(error, response, body){
+  console.log("entering Callback. " + response.statusCode);
     if(error) {
         console.log(error);
         return hook.res.write("Tropo Call Failed\n");
     } else {
-        console.log(response.statusCode, body);
-        return hook.res.write("Tropo Call Successful\n");
+
+          console.log("Tropo Results : " + response.statusCode);
+          return hook.res.write(response.statusCode + " : Tropo Results\n");
     }
 });
 }
